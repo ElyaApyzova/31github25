713e9df36ffe4e7ea23b878967adc044d26edfbe
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2023-12-23 23:51:10 +0300

    online shop

---
+++ src/components/Assets/data.js
@@ -0,0 +1,38 @@
+import img1 from "./photo1.png";
+import img2 from "./photo2.png";
+import img3 from "./photo3.png";
+import img4 from "./photo4.png";
+
+let data_product = [
+  {
+    id: 1,
+    name: "Shirt",
+    image: img1,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+
+  {
+    id: 1,
+    name: "Shirt",
+    image: img1,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+
+  {
+    id: 1,
+    name: "Shirt",
+    image: img1,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+
+  {
+    id: 1,
+    name: "Shirt",
+    image: img1,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+];
